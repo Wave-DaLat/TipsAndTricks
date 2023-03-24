5fa783fcb448efef89bbda87d0fa7bcf06b4539f
Wave <anhdalat.dlu@gmail.com>
2023-03-24 12:48:55 +0700

    Commit

--- TatBlog.WinApp/Program.cs
+++ TatBlog.WinApp/Program.cs
@@ -1,26 +1,28 @@
-﻿using TatBlog.Data.Contexts;
-using TatBlog.Services.Blogs;
-using TatBlog.WinApp;
+﻿//using TatBlog.Data.Contexts;
+//using TatBlog.Services.Blogs;
+//using TatBlog.WinApp;
 
-var context = new BlogDbContext();
+//var context = new BlogDbContext();
 
-IBlogRepository blogrepo = new BlogRepository(context);
+//IBlogRepository blogrepo = new BlogRepository(context);
 
-var pagingParams = new PagingParams
-{
-    PageNumber = 1,
-    PageSize = 20,
-    SortColumn = "name",
-    SortOrder = "desc",
-};
+//var pagingParams = new PagingParams
+//{
+//    PageNumber = 1,
+//    PageSize = 20,
+//    SortColumn = "name",
+//    SortOrder = "desc",
+//};
 
-var tagslist = await blogrepo.GetPagedTagsAsync(pagingParams);
+//var tagslist = await blogrepo.GetPagedTagsAsync(pagingParams);
 
-Console.WriteLine("{0, -5}{1, -50}{2, 10}",
-    "id", " name", "count");
+//Console.WriteLine("{0, -5}{1, -50}{2, 10}",
+//    "id", " name", "count");
 
-foreach (var item in tagslist)
-{
-    Console.WriteLine("{0, -5}{1, -50}{2, 10}",
-        item.Id, item.Name, item.PostCount);
-}
+//foreach (var item in tagslist)
+//{
+//    Console.WriteLine("{0, -5}{1, -50}{2, 10}",
+//        item.Id, item.Name, item.PostCount);
+//}
+
+Console.WriteLine("Tips & Tricks");
